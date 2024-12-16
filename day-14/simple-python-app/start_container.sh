6b20b27c7b0de9b0d0a89cde4e1dd9d1e3b59597
ars3232 <36531986+ars3232@users.noreply.github.com>
2024-12-16 22:01:24 +0600

    Update2 start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull aes3232/electra312:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 aes3232/electra312:latest
+
