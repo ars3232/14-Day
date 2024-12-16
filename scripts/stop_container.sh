210a8c11e8bd327af8b3f3fae2eefa01018e0efd
ars3232 <36531986+ars3232@users.noreply.github.com>
2024-12-16 22:25:47 +0600

    Update 6 stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop and remove all running containers
+docker stop $(docker ps -q) || true
+docker rm $(docker ps -aq) || true
